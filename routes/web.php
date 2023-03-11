adf3c18568a5e56035bfdff9192d11918b64892e
egatapova <gatapova.workspace21@gmail.com>
2023-03-11 13:59:56 +0300

    added Product table, seeder, controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
